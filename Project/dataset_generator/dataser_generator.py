c025fac08c846b05b050f3a68b9648363b3f3a60
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-12-02 11:09:43 +0100

    change folder name

--- Project/dataset_generator/dataser_generator.py
+++ Project/dataset_generator/dataser_generator.py
@@ -6,16 +6,12 @@ name_path = "./alexandra-baby-names/data/babynames_clean.csv"
 age_bound = [18, 105]
 n_entry = 200
 
-
-
 if __name__ == "__main__":
-    #load the name
+    # load the name
     df_name = pd.read_csv(name_path, header=0, names=['Name', 'Sex'])
     print(df_name.shape)
 
     for i in n_entry:
         new_entry = []
         k = random.randrange(0, df_name.shape[1])
-        pp = df_name.loc[k,:]
-
-
+        pp = df_name.loc[k, :]
