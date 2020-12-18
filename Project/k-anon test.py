277c6c1ad5b0439462960f75373b0407506436a4
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-12-18 10:00:22 +0100

    minor chnages

--- Project/k-anon test.py
+++ Project/k-anon test.py
@@ -1,16 +1,17 @@
 from numbers import Number
+
 from pandas.api.types import is_numeric_dtype
 
 
-def chose_dimension(dimensions):
-    return ""
+def chose_dimension(dimensions, step):
+    return step % len(dimensions)
 
 
 def compute_phi(partition, dim):
     return 0
 
 
-def find_median(partition, dim):
+def find_median(partition, dim, k):
     if is_numeric_dtype(partition[dim]):
         return partition[dim].median()
 
@@ -18,23 +19,23 @@ def find_median(partition, dim):
     return None
 
 
-def split_partition(partition, dim, split_val):
+def split_partition(partition, dim, split_val, k):
     if isinstance(split_val, Number):
         left_p = partition[partition[dim] >= split_val]
         right_p = partition[partition[dim] < split_val]
-    else:   # TODO: da gestire il caso in cui non sia un numero
+    else:  # TODO: da gestire il caso in cui non sia un numero
         left_p, right_p = None, None
 
     return left_p, right_p
 
 
-def anonymize(partition, columns):
+def anonymize(partition, columns, step, k):
     # If nor allowed multidimensional cut for partition
     #   return phi: partition -> summary
 
-    dim = chose_dimension(columns)
-    mean = find_median(partition, dim)
-    lhs, rhs = split_partition(partition, dim, mean)
+    dim = chose_dimension(columns, step)
+    mean = find_median(partition, dim, k)
+    lhs, rhs = split_partition(partition, dim, mean, k)
 
-    return [anonymize(lhs, columns),
-            anonymize(rhs, columns)]
+    return [anonymize(lhs, columns, step + 1, k),
+            anonymize(rhs, columns, step + 1, k)]
